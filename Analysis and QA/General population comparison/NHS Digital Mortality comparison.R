6c7c789ff718b3b5158069197d2f89e1a1791c94
Sebastien Peytrignet <Sebastien.Peytrignet@health.org.uk>
2021-06-22 14:29:40 +0100

    Mortality comparison up to Aug 2020

--- Analysis and QA/General population comparison/NHS Digital Mortality comparison.R
+++ Analysis and QA/General population comparison/NHS Digital Mortality comparison.R
@@ -37,11 +37,39 @@ numbers_only <- function(x) !grepl("\\D", x)
 ##################### Import NHS Digital data  #####################
 ####################################################################
 
-######################## Load NHS Digital data
+######################## Load NHS Digital data: Mortality
 
 nhsdigital.mortality <- fread(paste0(rawdatadir,"Tracking Healthcare Activity and Outcomes for Shielded Patients, England, Open Data.csv"),
                              header=TRUE, sep=",", check.names=T) %>%
   filter(.,Activity_type=="Mortality",Breakdown=="All",Rate!="NULL") %>%
+  mutate(.,Rate=as.numeric(Rate)) %>%
+  select(.,Population_group,Breakdown,Date,Rate,Numerator,Denominator) %>%
+  pivot_wider(
+    names_from = Population_group,
+    names_sep = ".",
+    values_from = c(Rate,Numerator,Denominator)
+  ) %>%
+  mutate(.,Date=lubridate::dmy(Date)) %>%
+  filter(.,Date<="2020-08-31")
+
+nhsdigital.mortality %>%
+  slice_max(.,SPL,n=1)
+
+#Deaths between 23/03/20 and 31/7/30
+nhsdigital.mortality %>%
+  pull(.,Numerator.SPL) %>%
+  as.numeric() %>%
+  sum(.)
+
+######################## Save
+
+fwrite(nhsdigital.mortality, file = paste0(onedrivedir,"nhsdigital.mortality.csv"), sep = ",")
+
+######################## Load NHS Digital data: COVID testing
+
+nhsdigital.testing <- fread(paste0(rawdatadir,"Tracking Healthcare Activity and Outcomes for Shielded Patients, England, Open Data.csv"),
+                              header=TRUE, sep=",", check.names=T) %>%
+  filter(.,Activity_type=="Pillar 1 testing",Breakdown=="COVID-19",Rate!="NULL") %>%
   mutate(.,Rate=as.numeric(Rate)) %>%
   select(.,Population_group,Breakdown,Date,Rate) %>%
   pivot_wider(
@@ -52,9 +80,12 @@ nhsdigital.mortality <- fread(paste0(rawdatadir,"Tracking Healthcare Activity an
   mutate(.,Date=lubridate::dmy(Date)) %>%
   filter(.,Date<="2020-07-31")
 
-nhsdigital.mortality %>%
+nhsdigital.testing %>%
   slice_max(.,SPL,n=1)
+
+nhsdigital.testing %>%
+  slice_max(.,`General population`,n=1)
 
 ######################## Save
 
-fwrite(nhsdigital.mortality, file = paste0(onedrivedir,"nhsdigital.mortality.csv"), sep = ",")
+fwrite(nhsdigital.testing, file = paste0(onedrivedir,"nhsdigital.testing.csv"), sep = ",")
